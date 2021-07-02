46aee20b51334c02e7d909694e7912102d5e80cd
Artem Chebotarev <artp956@gmail.com>
2021-07-02 11:04:40 +0300

    added products

---
+++ client/mealzers/src/layouts/Default.vue
@@ -0,0 +1,124 @@
+<!-- src/layouts/Default.vue -->
+  <template>
+  <v-app>
+    <div>
+      <v-app-bar
+        absolute
+        color="orange"
+        dark
+        shrink-on-scroll
+        scroll-target="#scrolling-techniques-2"
+      >
+        <v-app-bar-nav-icon class="mt-5" @click="drawer = !drawer">
+        </v-app-bar-nav-icon>
+        <g-link
+          class="mt-7 ml-3"
+          style="text-decoration: none; color: inherit"
+          to="/"
+          >Mealzers</g-link
+        >
+        <v-spacer></v-spacer>
+        <v-btn outlined rounded dense class="mt-5 mr-3">
+          <g-link style="text-decoration: none; color: inherit" to="/shop/"
+            >Shop</g-link
+          >
+        </v-btn>
+        <v-btn outlined rounded dense class="mt-5 mr-3">
+          <g-link style="text-decoration: none; color: inherit" to="/support/"
+            >Support</g-link
+          >
+        </v-btn>
+        <v-text-field
+          v-model="searchText"
+          @click:clear="searchText = ''"
+          placeholder="Search products ..."
+          class="mt-5 mr-4"
+          style="max-width: 350px"
+          prepend-inner-icon="mdi-magnify"
+          clearable
+          outlined
+          rounded
+          dense
+          hide-details
+        />
+        <v-btn class="mt-4 mr-4 ml-4 snipcart-checkout" icon>
+          <v-icon>mdi-cart</v-icon>
+          <span class="snipcart-total-items"></span>
+          <span class="snipcart-total-price">{{ this.totalPrice }}</span>
+        </v-btn>
+      </v-app-bar>
+      <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-title>
+                <v-btn elevation="0" rounded>
+                  <g-link
+                    style="text-decoration: none; color: orange"
+                    to="/shop/"
+                    >Shop</g-link
+                  >
+                </v-btn>
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>
+                <v-btn elevation="0" rounded dense>
+                  <g-link
+                    style="text-decoration: none; color: orange"
+                    to="/support/"
+                    >Support</g-link
+                  >
+                </v-btn>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+      <v-sheet
+        id="scrolling-techniques-2"
+        class="overflow-y-auto"
+        max-height="600"
+      >
+        <v-container style="height: 120px"></v-container>
+      </v-sheet>
+      <slot />
+    </div>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    Footer,
+  },
+
+  data() {
+    const drawer = false;
+    const group = null;
+    return {
+      drawer,
+      group,
+    };
+  },
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
---
+++ client/mealzers/src/pages/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <Layout>
+    <template>
+      <v-carousel
+        cycle
+        height="400"
+        hide-delimiter-background
+        :show-arrows="false"
+      >
+        <v-carousel-item v-for="(image, i) in images" :key="i">
+          <v-sheet height="100%">
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="display-3">
+                <img :src="image" />
+              </div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+      <div class="separator"></div>
+    </template>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Mealzers",
+  },
+
+  data() {
+    return {
+      images: [
+        "https://bit.ly/33Ehev5",
+        "https://bit.ly/33Ehev5",
+        "https://bit.ly/33Ehev5",
+        "https://bit.ly/33Ehev5",
+      ],
+    };
+  },
+};
+</script>
